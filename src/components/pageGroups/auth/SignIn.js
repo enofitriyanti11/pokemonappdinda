044abc4b5bf82f5152140ff6d2b46206dc247a93
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-03-24 16:42:52 +0700

    fix: add login user

--- src/components/pageGroups/auth/SignIn.js
+++ src/components/pageGroups/auth/SignIn.js
@@ -1,28 +1,47 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import "./SigninStyle.css";
+import { useNavigate } from 'react-router-dom';
 
-function SignIn(props) {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+function SignIn () {
+  let navigate = useNavigate();
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
+    e.preventDefault()
+    axios.post("https://reqres.in/api/login", { email, password })
+    .then((response) => {
+      console.log(response);
+      navigate('/');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
-
+  
   return (
-    <div class="py-28 mx-60">
-      <div class="bg-white/25 p-5 rounded-lg">
+    <div className="py-28 mx-20 md:mx-60">
+      <div className="bg-white/25 p-5 rounded-lg">
         <div className="auth-for-container">
-          <h4 class="uppercase text-[#424372] text-4xl font-bold py-3 text-center"> Sign In</h4>
+          <h4 className='uppercase text-[#424372] text-4xl font-bold py-3 text-center'> Sign In</h4>
           <form className="signin-form" onSubmit={handleSubmit}>
-            <label htmlfor="email">email</label>
-            <input class=" items-center" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlfor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <div class="my-3">
-              <button type="button" class="inline-block px-20 py-2.5 bg-[#8687bb] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#d4a695] hover:shadow-lg focus:bg-[#8687bb] focus:shadow-lg focus:outline-none focus:ring-0 active:[#d4a695] active:shadow-lg transition duration-150 ease-in-out">Login</button>
+            <label htmlFor="email">Email</label>
+            <input className='items-center' value={email} onChange={handleEmailChange} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={password} onChange={handlePasswordChange} type="password" placeholder="********" id="password" name="password" />
+            <div className='my-3'>
+              <button type="submit" className="inline-block px-20 py-2.5 bg-[#8687bb] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#d4a695] hover:shadow-lg focus:bg-[#8687bb] focus:shadow-lg focus:outline-none focus:ring-0 active:[#d4a695] active:shadow-lg transition duration-150 ease-in-out">Login</button>
             </div>
             <Link to="/SignUp">
               <button className="link-btn">Don't have an account? Sign Up</button>
