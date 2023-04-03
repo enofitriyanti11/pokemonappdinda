396228d2bae0f7132c938ef6c638a5aedb5616ab
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-04-03 13:41:31 +0700

    fix: detail pokemon using api

--- src/routes/Indexroutes.js
+++ src/routes/Indexroutes.js
@@ -3,7 +3,7 @@ import Home from "../pages/dashboard/Home";
 import Pokemons from "../pages/dashboard/Pokemons";
 import SignIn from "../components/pageGroups/auth/SignIn";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Id from '../components/pageGroups/dashboard/Pokemons/[id]';
+import DetailPokemons from '../components/pageGroups/dashboard/Pokemons/[id]';
 import Todos from '../components/pageGroups/dashboard/Todos';
 import Mypokemons from '../pages/dashboard/My-pokemons';
 import SignUp from '../components/pageGroups/auth/SignUp';
@@ -17,7 +17,7 @@ function Indexroutes() {
         <Route path="/" element={<Home />} />
         <Route path="/Pokemons" element={<Pokemons />} />
         <Route path="/Mypokemons" element={<Mypokemons/>} />
-        <Route path="/Id" element={<Id />} />
+        <Route path="/pokemons/:id" element={<DetailPokemons />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Todos" element={<Todos />} />
