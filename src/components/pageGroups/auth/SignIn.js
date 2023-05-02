039b7b568fd144c1b9ba3d342934829878bc78b2
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-02 11:14:14 +0700

    fix:edit mobile responsive SigIn

--- src/components/pageGroups/auth/SignIn.js
+++ src/components/pageGroups/auth/SignIn.js
@@ -36,17 +36,26 @@ function SignIn() {
   }
 
   return (
-    <div className="py-28 mx-20 md:mx-60">
-      <div className="bg-white/25 p-5 rounded-lg">
+    <div className="py-28 mx-20">
+      <div className="bg-white/25 p-5 mx-auto max-w-xs rounded-lg">
         <div className="auth-for-container">
-          <h4 className='uppercase text-[#424372] text-4xl font-bold py-3 text-center'> Sign In</h4>
+          <h4 className='uppercase text-[#424372] text-4xl font-bold py-3 text-center'>Login</h4>
           <form className="signin-form" onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
-            <input className='items-center' value={email} onChange={handleEmailChange} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input
+              style={{ width: '100%' }}
+              className='items-center'
+              value={email}
+              onChange={handleEmailChange}
+              type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
-            <input value={password} onChange={handlePasswordChange} type="password" placeholder="********" id="password" name="password" />
+            <input
+              style={{ width: '100%' }}
+              value={password} onChange={handlePasswordChange} type="password" placeholder="********" id="password" name="password" />
             <div className='my-3'>
-              <button type="submit" className="inline-block px-20 py-2.5 bg-[#8687bb] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#d4a695] hover:shadow-lg focus:bg-[#8687bb] focus:shadow-lg focus:outline-none focus:ring-0 active:[#d4a695] active:shadow-lg transition duration-150 ease-in-out">Login</button>
+              <button
+                type="submit"
+                className="inline-block px-20 py-2.5 bg-[#8687bb] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#d4a695] hover:shadow-lg focus:bg-[#8687bb] focus:shadow-lg focus:outline-none focus:ring-0 active:[#d4a695] active:shadow-lg transition duration-150 ease-in-out">Login</button>
             </div>
             <Link to="/SignUp">
               <button className="link-btn">Don't have an account? Sign Up</button>
